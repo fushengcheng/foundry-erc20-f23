71bac3283cb82ae6edabe0d5607d3f8fb97ca15b
fushengcheng <819796198@qq.com>
2023-09-13 14:50:59 +0800

    2

---
+++ test/OurTokenTest.t.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployOurToken} from "../script/DeployOurToken.s.sol";
+import {OurToken} from "../src/OurToken.sol";
+
+contract OurTokenTest is Test {
+    OurToken public ourToken;
+    DeployOurToken public deployer;
+
+    uint256 public constant STARTING_BALANCE = 100 ether;
+
+    address bob = makeAddr("bob");
+    address alice = makeAddr("alice");
+
+    function setUp() public {
+        deployer = new DeployOurToken();
+        ourToken = deployer.run();
+
+        vm.prank(msg.sender);
+        ourToken.transfer(bob, STARTING_BALANCE);
+    }
+
+    // 并没有写完...
+}
